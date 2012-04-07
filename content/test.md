6c5f7b0b25384d15319534de1a80087c8c012199
Dave Anderson <dave@electrobarn.com>
2012-04-07 15:43:17 -0600

    initial commit

---
+++ content/test.md
@@ -0,0 +1,6 @@
+Date: 2012-04-07
+Title: ElectroBarn
+Slug: electrobarn
+
+Just a test of this new blogging platform
+
---
+++ pelican.conf.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = u"Dave Anderson"
+SITENAME = u"electrobarn.com"
+SITEURL = 'http://electrobarn.com/'
+
+TIMEZONE = 'America/Edmonton'
+
+DEFAULT_LANG='en'
+
+GITHUB_URL = 'http://github.com/electrobarn/'
+
+FEED_RSS = 'feeds/all.rss.xml'
+CATEGORY_FEED_RSS = 'feeds/%s.rss.xml'
+
+THEME = 'bootstrap'
+
+# Blogroll
+LINKS =  (
+    ('Robots and Pencils', 'http://robotsandpencils.com'),
+         )
+
+# Social widget
+SOCIAL = (
+          ('twitter', 'http://twitter.com/electrobarn'),
+          ('github', 'http://github.com/electrobarn'),
+         )
+
+DEFAULT_PAGINATION = False
+
+
+    
